e6c9328df47b3b88ec097082951a5f7f013e5cdd
Rupesh hayasis <chrupesh@hayasis.com>
2025-08-11 11:40:52 +0530

    app.js

---
+++ client/src/pages/StatisticsPage.js
@@ -0,0 +1,16 @@
+import React from "react";  
+import { Typography, Box } from "@mui/material";        
+function StatisticsPage() { 
+    return (
+        <Box sx={{ mt: 4 }}>
+            <Typography variant="h4" gutterBottom>
+                Statistics
+            </Typography>
+            <Typography>
+              URL statistics here
+            </Typography>
+        </Box>
+    );
+}
+
+export default StatisticsPage;
--- client/src/pages/UrlShortenerPage.js
+++ client/src/pages/UrlShortenerPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Typography,Box } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 function UrlShortenerPage() {
   return (
-    <Box>
-      <Typography variant="h4">URL Shortener</Typography>
-      {/* Add your URL shortener form and functionality here */}
+    <Box sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        URL Shortener
+      </Typography>
+      <Typography>
+        Create up to 5 shortened URLs here...
+      </Typography>
     </Box>
   );
 }
+
+export default UrlShortenerPage;
